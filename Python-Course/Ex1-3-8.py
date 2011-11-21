26674b89263c9556b6bb7b02e6ad4b71ac06ff47
Dan Wilbraham <dwilbraham+git@gmail.com>
2011-11-21 14:21:30 +0000

    First section of Ex 1-3-8

--- Python-Course/Ex1-3-8.py
+++ Python-Course/Ex1-3-8.py
@@ -1,2 +1,18 @@
+import time
 import timeit
-print (timeit.default_timer)
+
+def time_call(func):
+    def _time_call(*args, **kwargs):
+        start = timeit.default_timer()
+        ret = func(*args, **kwargs)
+        print ("Function call time = " + str(timeit.default_timer() - start))
+        return ret
+    return _time_call
+
+@time_call
+def sleep_for(timeToSleep):
+    time.sleep(timeToSleep)
+
+if __name__ == '__main__':
+    print (timeit.default_timer())
+    sleep_for(5)
