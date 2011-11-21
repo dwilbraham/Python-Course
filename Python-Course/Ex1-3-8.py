09cc11b082d12eec06d4164865c324791f2f08c5
Dan Wilbraham <dwilbraham+git@gmail.com>
2011-11-21 15:08:48 +0000

    Finished Ex 1-3-8.
    Also checkin notes from this section.

--- Python-Course/Ex1-3-8.py
+++ Python-Course/Ex1-3-8.py
@@ -2,22 +2,23 @@ import time
 import timeit
 import functools
 
-def time_call(timesRun):
+def time_call(timesToRun):
     def _time_call(func):
         @functools.wraps(func)
         def __time_call(*args, **kwargs):
             start = timeit.default_timer()
-            ret = func(*args, **kwargs)
-            print ("Function call time = " + str((timeit.default_timer() - start) / timesRun))
+            for run in xrange(timesToRun):
+                ret = func(*args, **kwargs)
+            print ("Function call time = " + str((timeit.default_timer() - start) / timesToRun))
             return ret
         return __time_call
     return _time_call
 
-@time_call(2)
+@time_call(1000)
 def sleep_for(timeToSleep):
     """Sleeps for the given number of seconds"""
     time.sleep(timeToSleep)
 
 if __name__ == '__main__':
 #    print (timeit.default_timer())
-    sleep_for(5)
+    sleep_for(0.01)
