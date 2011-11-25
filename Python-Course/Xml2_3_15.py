b69b97eec62802990dbde40cab195391f9b1db25
Dan Wilbraham <dwilbraham+git@gmail.com>
2011-11-25 12:19:55 +0000

    Add Ex Xml2.3.15

---
+++ Python-Course/Xml2_3_15.py
@@ -0,0 +1,15 @@
+import lxml.etree as ET
+
+if __name__ == '__main__':
+    tree = ET.parse('xml/cityindex.xml')
+    
+    print([ el.text for el in tree.xpath('//city')])
+    
+    find_city = ET.XPath('/cityindex/entry/region')
+    regionList = [el.text for el in find_city(tree)]
+    regionCount = {}
+    for i in regionList:
+        regionCount.setdefault(i, 0)
+        regionCount[i] =+ 1
+    print(regionCount)
+    
